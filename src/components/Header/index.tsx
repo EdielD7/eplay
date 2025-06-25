17fc36376192d707b9d75eec552c1c573506ca46
EdielD7 <ediel.purim@hotmail.com>
2025-06-24 22:22:00 -0400

    aula 35.8

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,9 @@ import carrinho from '../../assets/images/carrinho.svg'
 const Header = () => (
   <HeaderBar>
     <div>
-      <img src={logo} alt="eplay" />
+      <Link to="/">
+        <img src={logo} alt="eplay" />
+      </Link>
       <nav>
         <Links>
           <LinkItem>
