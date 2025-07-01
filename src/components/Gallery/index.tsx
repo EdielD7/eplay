aa38e075150b292189616f83532987729f47e667
EdielD7 <ediel.purim@hotmail.com>
2025-07-01 03:02:40 -0400

    36.8 Integra com API - II

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -12,31 +12,32 @@ import play from '../../assets/images/play.png'
 import zoom from '../../assets/images/zoom.png'
 import fechar from '../../assets/images/fechar.png'
 
-const mock: GalleryItem[] = [
-  {
-    type: 'image',
-    url: spiderman
-  },
-  {
-    type: 'image',
-    url: hogwarts
-  },
-  {
-    type: 'video',
-    url: 'https://www.youtube.com/embed/uHGShqcAHlQ'
-  }
-]
+// const mock: [] = [
+//   {
+//     type: 'image',
+//     url: spiderman
+//   },
+//   {
+//     type: 'image',
+//     url: hogwarts
+//   },
+//   {
+//     type: 'video',
+//     url: 'https://www.youtube.com/embed/uHGShqcAHlQ'
+//   }
+// ]
 
 type Props = {
   defaultCover: string
   name: string
+  items: GalleryItem[]
 }
 
 interface ModalState extends GalleryItem {
   isVisible: boolean
 }
 
-const Gallery = ({ defaultCover, name }: Props) => {
+const Gallery = ({ defaultCover, name, items }: Props) => {
   const [modal, setModal] = useState<ModalState>({
     type: 'image',
     url: '',
@@ -65,7 +66,7 @@ const Gallery = ({ defaultCover, name }: Props) => {
     <>
       <Section title="Galeria" background="black">
         <Items>
-          {mock.map((media, index) => (
+          {items.map((media, index) => (
             <Item
               key={media.url}
               onClick={() => {
