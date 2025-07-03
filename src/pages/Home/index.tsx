d277127ccdc36d3601788e8e6e044e68ebcfcf74
EdielD7 <ediel.purim@hotmail.com>
2025-07-03 12:43:41 -0400

    37.1 configura redux

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,11 +3,6 @@ import { useEffect, useState } from 'react'
 import Banner from '../../components/Banner'
 import ProductsList from '../../components/ProductsList'
 
-import starWars from '../../assets/images/star_wars.png'
-import diablo from '../../assets/images/diablo.png'
-import zelda from '../../assets/images/zelda.png'
-import resident from '../../assets/images/resident.png'
-
 export interface GalleryItem {
   type: 'image' | 'video'
   url: string
